1b3cf297f2065bfac025636b30f7702c1668be20
Artur Dorobek <artur.dorobek@gmail.com>
2023-03-30 20:42:55 +0200

    Add new class EstateConfiguration

---
+++ RealEstate.Persistance/Configuration/EstateConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RealEstate.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Persistance.Configuration
+{
+    public class EstateConfiguration : IEntityTypeConfiguration<Estate>
+    {
+        public void Configure(EntityTypeBuilder<Estate> builder)
+        {
+            builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(3000).IsRequired();
+        }
+    }
+}
