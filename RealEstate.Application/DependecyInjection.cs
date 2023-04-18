7f9fcd1a6268ed373721028f579af7503da7008f
art772 <art772@wp.pl>
2023-04-18 19:41:34 +0200

    Add LoggingBehaviour - to Application\Common\Behaviours

--- RealEstate.Application/DependecyInjection.cs
+++ RealEstate.Application/DependecyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
+using MediatR.Pipeline;
 using Microsoft.Extensions.DependencyInjection;
+using RealEstate.Application.Common.Behaviours;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,8 @@ namespace RealEstate.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IRequestPreProcessor<>), typeof(LoggingBehaviour<>));
             return services;
         }
     }
