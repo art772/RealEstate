0fb88c4026689369d87d1e3d4e0f31b47b903fc5
art772 <art772@wp.pl>
2023-06-05 22:12:53 +0200

    First version of Restore Estate - Created RestoreEstateCommand and RestoreEstateCommandHandler

---
+++ RealEstate.Application/Estates/Commands/RestoreEstate/RestoreEstateCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Application.Estates.Commands.RestoreEstate
+{
+    public class RestoreEstateCommand : IRequest<int>
+    {
+        public int EstateId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RealEstate.Application/Estates/Commands/RestoreEstate/RestoreEstateCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RealEstate.Application.Common.Interfaces;
+using RealEstate.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Application.Estates.Commands.RestoreEstate
+{
+    public class RestoreEstateCommandHandler : IRequestHandler<RestoreEstateCommand, int>
+    {
+        private readonly IEstateDbContext _context;
+        public async Task<int> Handle(RestoreEstateCommand request, CancellationToken cancellationToken)
+        {
+            var estate = await _context.Estates.Where(p => p.Id == request.EstateId).FirstOrDefaultAsync(cancellationToken);
+
+            if (estate != null)
+            {
+                estate.StatusId = 1;
+            }
+            else
+            {
+                throw new Exception($"Estate with this Id: {request.EstateId} is not deleted");
+            }
+
+            _context.Estates.Add(estate);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return estate.Id;
+        }
+    }
+}
