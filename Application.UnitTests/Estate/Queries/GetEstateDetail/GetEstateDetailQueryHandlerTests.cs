e17e24aaab4105aebf571aa156f2df75b8f79cb0
art772 <art772@wp.pl>
2023-06-08 20:38:25 +0200

    Created - Unit test -  GetEstatedDetail

--- Application.UnitTests/Estate/Queries/GetEstateDetail/GetEstateDetailQueryHandlerTests.cs
+++ Application.UnitTests/Estate/Queries/GetEstateDetail/GetEstateDetailQueryHandlerTests.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Application.UnitTests.Common;
+using RealEstate.Application.Estates.Queries.GetEstateDetail;
+using RealEstate.Application.Estates.Queries.GetEstates;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Application.UnitTests.Estate.Queries.GetEstateDetail
 {
-    public class GetEstateDetailQueryHandlerTests
+    public class GetEstateDetailQueryHandlerTests : CommandTestBase
     {
+        private readonly GetEstateDetailQueryHandler _handler;
+
+        public GetEstateDetailQueryHandlerTests() : base()
+        {
+            _handler = new GetEstateDetailQueryHandler(_context);
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidRequest_ShouldGetEstateDetailId1()
+        {
+            var query = new GetEstateDetailQuery() { EstateId = 1 };
+
+            await _handler.Handle(query, CancellationToken.None);
+        }
     }
 }
--- Application.UnitTests/Estate/Queries/GetEstates/GetEstatesListQueryHandlerTests.cs
+++ Application.UnitTests/Estate/Queries/GetEstates/GetEstatesListQueryHandlerTests.cs
@@ -1,14 +1,6 @@
 ﻿using Application.UnitTests.Common;
-using Microsoft.EntityFrameworkCore;
 using RealEstate.Application.Estates.Queries.GetEstates;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using Xunit;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Application.UnitTests.Estate.Queries.GetEstates
 {
