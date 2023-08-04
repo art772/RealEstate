66301b9798afc116cf0ce3c98530a9d70cacd7ce
art772 <art772@wp.pl>
2023-08-04 23:23:51 +0200

    Connected Web Api with Angular

--- RealEstate/Program.cs
+++ RealEstate/Program.cs
@@ -58,6 +58,8 @@ builder.Services.AddAuthentication(options =>
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(option =>
@@ -109,6 +111,9 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
