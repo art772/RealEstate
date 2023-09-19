08805ca580a34ec3893a1d11b851b158b62aae79
art772 <art772@wp.pl>
2023-09-19 22:50:06 +0200

    Added class to validate data while Create/Update Category, Genre, State

---
+++ RealEstate.Application/States/Commands/UpdateState/UpdateStateCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate.Application.States.Commands.UpdateState
+{
+    internal class UpdateStateCommandValidator
+    {
+    }
+}
